010b45c7e639234a957c0607f0b0fd26daa8203f
ScorpionInc <d2dejohn@gmail.com>
2025-06-12 14:19:18 -0400

    Update si_queue_test.c
    Better test.

--- CProjectTemplate/si_queue_test.c
+++ CProjectTemplate/si_queue_test.c
@@ -5,7 +5,17 @@ int main(void)
 	si_queue q = {};
 	init_si_queue(&q);
 	char c = 'a';
-	si_queue_enqueue(&q, &c);
+	for(int i = 0; i < 6; i++)
+	{
+		si_queue_enqueue(&q, &c);
+		c += 1u;
+		printf("Queue Count: %lu\tCapacity: %lu\n", si_queue_count(&q), q.dynamic.capacity);
+	}
+	for(int i = 0; i < 6; i++)
+	{
+		si_queue_dequeue(&q, &c);
+		printf("Popped char: %c\tNew Count: %lu.\n", c, si_queue_count(&q));
+	}
 	free_si_queue(&q);
 	return 0;
 }
