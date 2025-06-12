5d328f838270cca438e58082bce33128747f647c
ScorpionInc <d2dejohn@gmail.com>
2025-06-12 14:19:55 -0400

    Cleanup
    Changed comments and added parameter to valgrind for test script.

--- CProjectTemplate/test.sh
+++ CProjectTemplate/test.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 echo 'Testing SiArray_test.c'
 gcc -Wall ./SiArray_test.c -o test.elf -lm &&
-valgrind --leak-check=full --track-origins=yes -s ./test.elf &&
+valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes -s ./test.elf &&
 rm -f ./test.elf
 echo
 
 echo 'Testing si_bits_test.c'
 gcc -Wall ./si_bits.c ./si_bits_tests.c -o test.elf &&
-valgrind --leak-check=full --track-origins=yes -s ./test.elf &&
+valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes -s ./test.elf &&
 rm -f ./test.elf
 echo
 
 echo 'Testing si_queue_test.c'
 gcc -Wall ./si_dynamic.c ./si_queue.c ./si_queue_test.c -o test.elf -lm &&
-valgrind --leak-check=full --track-origins=yes -s ./test.elf &&
+valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes -s ./test.elf &&
 rm -f ./test.elf
 echo
