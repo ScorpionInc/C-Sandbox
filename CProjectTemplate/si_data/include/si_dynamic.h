f45aa125d64fa0c50b77cc3a626f69a575988e4d
ScorpionInc <d2dejohn@gmail.com>
2025-07-24 11:30:41 -0400

    Update si_dynamic.h
    Missed one during the _t update.

--- CProjectTemplate/si_data/include/si_dynamic.h
+++ CProjectTemplate/si_data/include/si_dynamic.h
@@ -3,7 +3,7 @@
  * Authors: ScorpionInc
  * Purpose: Defines struct with functions for managing a dynamically allocated buffer.
  * Created: 20150501
- * Updated: 20250623
+ * Updated: 20250708
 //*/
 
 #include <stdbool.h>
@@ -20,12 +20,12 @@
 extern "C" {
 #endif //__cplusplus
 
-typedef struct si_dynamic
+typedef struct si_dynamic_t
 {
 	void* data;
 	size_t element_size;
 	size_t capacity;
-} si_dynamic;
+} si_dynamic_t;
 
 /** Doxygen
  * @brief Initializes fields within si_dynamic struct being pointed at.
@@ -35,9 +35,9 @@ typedef struct si_dynamic
  * @param capacity Number of said item to be stored in the buffer.
  * @param settings si_realloc_settings determining how to reallocate.
  */
-void si_dynamic_new_3(si_dynamic* p_dynamic, const size_t element_size,
+void si_dynamic_new_3(si_dynamic_t* p_dynamic, const size_t element_size,
 	const size_t capacity);
-void si_dynamic_new (si_dynamic* p_dynamic, const size_t element_size);
+void si_dynamic_new (si_dynamic_t* p_dynamic, const size_t element_size);
 
 /** Doxygen
  * @brief Calculates the size in bytes allocated for struct at p_dynamic
@@ -45,7 +45,7 @@ void si_dynamic_new (si_dynamic* p_dynamic, const size_t element_size);
  * @param p_dynamic Pointer to si_dynamic struct to size.
  * @return Returns the allocated size of the buffer in bytes.
  */
-size_t si_dynamic_size(const si_dynamic* p_dynamic);
+size_t si_dynamic_size(const si_dynamic_t* p_dynamic);
 
 /* Doxygen
  * @brief Sets capacity of buffer of items to new_capacity.
@@ -55,7 +55,7 @@ size_t si_dynamic_size(const si_dynamic* p_dynamic);
  *
  * @return Returns true on success. False otherwise.
  */
-bool si_dynamic_resize(si_dynamic* p_dynamic,
+bool si_dynamic_resize(si_dynamic_t* p_dynamic,
 	const size_t new_capacity);
 
 /** Doxygen
@@ -66,7 +66,7 @@ bool si_dynamic_resize(si_dynamic* p_dynamic,
  *
  * @return Returns bool true if true. Return false otherwise.
  */
-bool si_dynamic_is_pointer_within(const si_dynamic* p_dynamic,
+bool si_dynamic_is_pointer_within(const si_dynamic_t* p_dynamic,
 	const void* p_test);
 
 /** Doxygen
@@ -77,7 +77,7 @@ bool si_dynamic_is_pointer_within(const si_dynamic* p_dynamic,
  *
  * @return On good returns offset of pointer in heap. SIZE_MAX on failure/max.
  */
-size_t si_dynamic_find_pointer_offset(const si_dynamic* p_dynamic,
+size_t si_dynamic_find_pointer_offset(const si_dynamic_t* p_dynamic,
 	const void* p_test);
 
 /** Doxygen
@@ -88,7 +88,7 @@ size_t si_dynamic_find_pointer_offset(const si_dynamic* p_dynamic,
  *
  * @return Returns bool true if true. Return false otherwise.
  */
-bool si_dynamic_is_pointer_element(const si_dynamic* p_dynamic,
+bool si_dynamic_is_pointer_element(const si_dynamic_t* p_dynamic,
 	const void* p_test);
 
 /** Doxygen
@@ -99,7 +99,7 @@ bool si_dynamic_is_pointer_element(const si_dynamic* p_dynamic,
  *
  * @return On good returns index of element in heap. SIZE_MAX on failure/max.
  */
-size_t si_dynamic_find_pointer_index(const si_dynamic* p_dynamic,
+size_t si_dynamic_find_pointer_index(const si_dynamic_t* p_dynamic,
 	const void* p_test);
 
 /** Doxygen
@@ -110,7 +110,7 @@ size_t si_dynamic_find_pointer_index(const si_dynamic* p_dynamic,
  *
  * @return Success returns pointer into heap memory of element_size. Else NULL.
  */
-void* si_dynamic_at(const si_dynamic* p_dynamic,
+void* si_dynamic_at(const si_dynamic_t* p_dynamic,
 	const size_t index);
 
 /** Doxygen
@@ -120,7 +120,7 @@ void* si_dynamic_at(const si_dynamic* p_dynamic,
  *
  * @return Returns pointer into heap memory of element size on success. Or NUll
  */
-void* si_dynamic_first(const si_dynamic* p_dynamic);
+void* si_dynamic_first(const si_dynamic_t* p_dynamic);
 
 /** Doxygen
  * @brief Gets and returns last elements address of allocated memory.
@@ -129,7 +129,7 @@ void* si_dynamic_first(const si_dynamic* p_dynamic);
  *
  * @return Returns pointer into heap memory of element size on success. Or NUll
  */
-void* si_dynamic_last(const si_dynamic* p_dynamic);
+void* si_dynamic_last(const si_dynamic_t* p_dynamic);
 
 /** Doxygen
  * @brief Assigns aligned bytes inside allocated buffer to p_item bytes @ index
@@ -138,7 +138,7 @@ void* si_dynamic_last(const si_dynamic* p_dynamic);
  * @param index Offset in blocks of element_size into buffer to start setting.
  * @param p_item Where to read the bytes from that will be used in buffer.
  */
-void si_dynamic_set(si_dynamic* p_dynamic,
+void si_dynamic_set(si_dynamic_t* p_dynamic,
 	const size_t index, const void* p_item);
 
 /** Doxygen
@@ -148,7 +148,7 @@ void si_dynamic_set(si_dynamic* p_dynamic,
  * @param index Offset in blocks of element_size into buffer to start setting.
  * @param p_item Where to read the bytes from that will be used in buffer.
  */
-void si_dynamic_get(const si_dynamic* p_dynamic,
+void si_dynamic_get(const si_dynamic_t* p_dynamic,
 	const size_t index, void* p_item);
 
 /** Doxygen
@@ -159,8 +159,8 @@ void si_dynamic_get(const si_dynamic* p_dynamic,
  *
  * @return Returns int value of 0 if equal. 1 or -1 otherwise simular to memcmp
  */
-int si_dynamic_cmp(const si_dynamic* const p_dynamic_a,
-	const si_dynamic* const p_dynamic_b);
+int si_dynamic_cmp(const si_dynamic_t* const p_dynamic_a,
+	const si_dynamic_t* const p_dynamic_b);
 
 /** Doxygen
  * @brief Writes values of si_dynamic struct to file for debugging.
@@ -168,14 +168,14 @@ int si_dynamic_cmp(const si_dynamic* const p_dynamic_a,
  * @param p_file Pointer to the FILE to be printed to.
  * @param p_dynamic Pointer to si_dynamic struct that is to be printed to file.
  */
-void fprint_si_dynamic(FILE* const p_file, const si_dynamic* const p_dynamic);
+void fprint_si_dynamic(FILE* const p_file, const si_dynamic_t* const p_dynamic);
 
 /** Doxygen
  * @brief Frees buffer if allocated.
  *
  * @param p_dynamic Pointer to si_dynamic struct that's to have it's data freed
  */
-void si_dynamic_free(si_dynamic* const p_dynamic);
+void si_dynamic_free(si_dynamic_t* const p_dynamic);
 
 #ifdef __cplusplus
 }
