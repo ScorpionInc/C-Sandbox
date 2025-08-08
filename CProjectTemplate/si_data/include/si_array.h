e8967742b4cf8117873c1ab27735bb29be6f2bdd
ScorpionInc <d2dejohn@gmail.com>
2025-08-08 10:18:59 -0400

    BREAKING Changed si_array new/init
    si_array_new didn't allocate and only initialized. new has been re-added and now allocated while the old new is now init(). Also fixed p_dynamic to now reflect refactored name of si_array

--- CProjectTemplate/si_data/include/si_array.h
+++ CProjectTemplate/si_data/include/si_array.h
@@ -28,154 +28,165 @@ typedef struct si_array_t
 } si_array_t;
 
 /** Doxygen
- * @brief Initializes fields within si_dynamic struct being pointed at.
+ * @brief Initializes fields within si_array_t struct being pointed at.
  *
- * @param p_dynamic Pointer addressing struct to be initialized.
+ * @param p_array Pointer addressing struct to be initialized.
  * @param element_size Size of a single item in the dynamic buffer.
  * @param capacity Number of said item to be stored in the buffer.
  * @param settings si_realloc_settings determining how to reallocate.
  */
-void si_array_new_3(si_array_t* p_dynamic, const size_t element_size,
+void si_array_init_3(si_array_t* p_array, const size_t element_size,
 	const size_t capacity);
-void si_array_new (si_array_t* p_dynamic, const size_t element_size);
+void si_array_init(si_array_t* p_array, const size_t element_size);
 
 /** Doxygen
- * @brief Calculates the size in bytes allocated for struct at p_dynamic
+ * @brief Allocates & initializes a new si_array_t struct.
+ * 
+ * @param element_size Size of a single item in the dynamic buffer.
+ * @param capacity Number of items in the buffer.
+ * 
+ * @return Returns pointer to newly allocated si_array_t struct in heap.
+ */
+si_array_t* si_array_new_2(const size_t element_size, const size_t capacity);
+si_array_t* si_array_new(const size_t element_size);
+
+/** Doxygen
+ * @brief Calculates the size in bytes allocated for struct at p_array
  *
- * @param p_dynamic Pointer to si_dynamic struct to size.
+ * @param p_array Pointer to si_array_t struct to size.
  * @return Returns the allocated size of the buffer in bytes.
  */
-size_t si_array_size(const si_array_t* p_dynamic);
+size_t si_array_size(const si_array_t* p_array);
 
 /* Doxygen
  * @brief Sets capacity of buffer of items to new_capacity.
  *
- * @param p_dynamic Pointer to si_dynamic struct to have a resized capacity.
+ * @param p_array Pointer to si_array_t struct to have a resized capacity.
  * @param new_capacity Number of items of element_size to be stored in buffer.
  *
  * @return Returns true on success. False otherwise.
  */
-bool si_array_resize(si_array_t* p_dynamic,
+bool si_array_resize(si_array_t* p_array,
 	const size_t new_capacity);
 
 /** Doxygen
  * @brief Determines if pointer lies with allocated data/buffer address space.
  *
- * @param p_dynamic Pointer to the struct to use for bounds check.
+ * @param p_array Pointer to the struct to use for bounds check.
  * @param p_test Pointer to check if it is within allocated memory.
  *
  * @return Returns bool true if true. Return false otherwise.
  */
-bool si_array_is_pointer_within(const si_array_t* p_dynamic,
+bool si_array_is_pointer_within(const si_array_t* p_array,
 	const void* p_test);
 
 /** Doxygen
  * @brief Calculates the byte offset into the buffer of the pointer.
  *
- * @param p_dynamic Pointer to si_dynamic struct to do checks with.
+ * @param p_array Pointer to si_array_t struct to do checks with.
  * @param p_test Pointer to allocated memory to do checks on.
  *
  * @return On good returns offset of pointer in heap. SIZE_MAX on failure/max.
  */
-size_t si_array_find_pointer_offset(const si_array_t* p_dynamic,
+size_t si_array_find_pointer_offset(const si_array_t* p_array,
 	const void* p_test);
 
 /** Doxygen
  * @brief Determines if pointer lies at an element starting address.
  *
- * @param p_dynamic Pointer to the struct to use for bounds check.
+ * @param p_array Pointer to the struct to use for bounds check.
  * @param p_test Pointer to check if it is pointing at an element.
  *
  * @return Returns bool true if true. Return false otherwise.
  */
-bool si_array_is_pointer_element(const si_array_t* p_dynamic,
+bool si_array_is_pointer_element(const si_array_t* p_array,
 	const void* p_test);
 
 /** Doxygen
  * @brief Returns the index of pointer or SIZE_MAX if invalid.
  *
- * @param p_dynamic Pointer to si_dynamic struct to do checks with.
+ * @param p_array Pointer to si_array_t struct to do checks with.
  * @param p_test Pointer to allocated memory to do checks on.
  *
  * @return On good returns index of element in heap. SIZE_MAX on failure/max.
  */
-size_t si_array_find_pointer_index(const si_array_t* p_dynamic,
+size_t si_array_find_pointer_index(const si_array_t* p_array,
 	const void* p_test);
 
 /** Doxygen
  * @brief Determines where in allocated memory element index starts.
  *
- * @param p_dynamic Pointer to si_dynamic struct
+ * @param p_array Pointer to si_array_t struct
  * @param index Index of the element in the buffer to find address of.
  *
  * @return Success returns pointer into heap memory of element_size. Else NULL.
  */
-void* si_array_at(const si_array_t* p_dynamic,
+void* si_array_at(const si_array_t* p_array,
 	const size_t index);
 
 /** Doxygen
  * @brief Gets and returns first elements address of allocated memory.
  *
- * @param p_dynamic Pointer to si_dynamic struct to read from.
+ * @param p_array Pointer to si_array_t struct to read from.
  *
  * @return Returns pointer into heap memory of element size on success. Or NUll
  */
-void* si_array_first(const si_array_t* p_dynamic);
+void* si_array_first(const si_array_t* p_array);
 
 /** Doxygen
  * @brief Gets and returns last elements address of allocated memory.
  *
- * @param p_dynamic Pointer to si_dynamic struct to read from.
+ * @param p_array Pointer to si_array_t struct to read from.
  *
  * @return Returns pointer into heap memory of element size on success. Or NUll
  */
-void* si_array_last(const si_array_t* p_dynamic);
+void* si_array_last(const si_array_t* p_array);
 
 /** Doxygen
  * @brief Assigns aligned bytes inside allocated buffer to p_item bytes @ index
  *
- * @param p_dynamic Pointer to si_dynamic struct whose buffer is to be changed.
+ * @param p_array Pointer to si_array_t struct whose buffer is to be changed.
  * @param index Offset in blocks of element_size into buffer to start setting.
  * @param p_item Where to read the bytes from that will be used in buffer.
  */
-void si_array_set(si_array_t* p_dynamic,
+void si_array_set(si_array_t* p_array,
 	const size_t index, const void* p_item);
 
 /** Doxygen
  * @brief Assigns aligned bytes inside allocated buffer to p_item bytes @ index
  *
- * @param p_dynamic Pointer to si_dynamic struct whose buffer is to be changed.
+ * @param p_array Pointer to si_array_t struct whose buffer is to be changed.
  * @param index Offset in blocks of element_size into buffer to start setting.
  * @param p_item Where to read the bytes from that will be used in buffer.
  */
-void si_array_get(const si_array_t* p_dynamic,
+void si_array_get(const si_array_t* p_array,
 	const size_t index, void* p_item);
 
 /** Doxygen
- * @brief Compares the raw byte values of two si_dynamic structs data.
+ * @brief Compares the raw byte values of two si_array_t structs data.
  *
- * @param p_dynamic_a Pointer to first si_dynamic struct to be compared.
- * @param p_dynamic_b Pointer to the second si_dynamic struct to be compared.
+ * @param p_array_a Pointer to first si_array_t struct to be compared.
+ * @param p_array_b Pointer to the second si_array_t struct to be compared.
  *
  * @return Returns int value of 0 if equal. 1 or -1 otherwise simular to memcmp
  */
-int si_array_cmp(const si_array_t* const p_dynamic_a,
-	const si_array_t* const p_dynamic_b);
+int si_array_cmp(const si_array_t* const p_array_a,
+	const si_array_t* const p_array_b);
 
 /** Doxygen
- * @brief Writes values of si_dynamic struct to file for debugging.
+ * @brief Writes values of si_array_t struct to file for debugging.
  *
  * @param p_file Pointer to the FILE to be printed to.
- * @param p_dynamic Pointer to si_dynamic struct that is to be printed to file.
+ * @param p_array Pointer to si_array_t struct that is to be printed to file.
  */
-void fprint_si_dynamic(FILE* const p_file, const si_array_t* const p_dynamic);
+void fprint_si_array(FILE* const p_file, const si_array_t* const p_array);
 
 /** Doxygen
  * @brief Frees buffer if allocated.
  *
- * @param p_dynamic Pointer to si_dynamic struct that's to have it's data freed
+ * @param p_array Pointer to si_array_t struct that's to have it's data freed
  */
-void si_array_free(si_array_t* const p_dynamic);
+void si_array_free(si_array_t* const p_array);
 
 #ifdef __cplusplus
 }
--- CProjectTemplate/si_data/src/si_array.c
+++ CProjectTemplate/si_data/src/si_array.c
@@ -6,93 +6,111 @@
 extern "C" {
 #endif //__cplusplus
 
-void si_array_new_3(si_array_t* p_dynamic, const size_t element_size,
+void si_array_init_3(si_array_t* p_array, const size_t element_size,
 	const size_t capacity)
 {
-	if (NULL == p_dynamic)
+	if (NULL == p_array)
 	{
 		goto END;
 	}
 	// Begin
-	p_dynamic->p_data = calloc(capacity, element_size);
-	if (NULL == p_dynamic->p_data)
+	p_array->p_data = calloc(capacity, element_size);
+	if (NULL == p_array->p_data)
 	{
 		goto END;
 	}
-	p_dynamic->element_size = element_size;
-	p_dynamic->capacity = capacity;
+	p_array->element_size = element_size;
+	p_array->capacity = capacity;
 	// End
 	END:
 		return;
 }
-inline void si_array_new(si_array_t* p_dynamic, const size_t element_size)
+inline void si_array_init(si_array_t* p_array, const size_t element_size)
 {
 	// Default value of capacity is 0u
-	si_array_new_3(p_dynamic, element_size, 0u);
+	si_array_init_3(p_array, element_size, 0u);
 }
 
-size_t si_array_size(const si_array_t* p_dynamic)
+si_array_t* si_array_new_2(const size_t element_size, const size_t capacity)
 {
-	size_t result = 0u;
-	// Validate Parameters
-	if (NULL == p_dynamic)
+	si_array_t* p_new = NULL;
+	p_new = calloc(1u, sizeof(si_array_t));
+	if(NULL == p_new)
 	{
 		goto END;
 	}
-	if ((NULL == p_dynamic->p_data) || (0u == p_dynamic->element_size))
+	si_array_init_3(p_new, element_size, capacity);
+END:
+	return p_new;
+}
+inline si_array_t* si_array_new(const size_t element_size)
+{
+	// Default value of capacity is 0u
+	return si_array_new_2(element_size, 0u);
+}
+
+size_t si_array_size(const si_array_t* p_array)
+{
+	size_t result = 0u;
+	// Validate Parameters
+	if (NULL == p_array)
+	{
+		goto END;
+	}
+	if ((NULL == p_array->p_data) || (0u == p_array->element_size))
 	{
 		goto END;
 	}
 	// Begin
 	// Prevent Overflows
-	if ((p_dynamic->capacity * p_dynamic->element_size) /
-		p_dynamic->element_size != p_dynamic->capacity)
+	if ((p_array->capacity * p_array->element_size) /
+		p_array->element_size != p_array->capacity)
 	{
 		// Overflow detected.
 		goto END;
 	}
-	result = p_dynamic->element_size * p_dynamic->capacity;
+	result = p_array->element_size * p_array->capacity;
 	// End
 END:
 	return result;
 }
 
-bool si_array_resize(si_array_t* p_dynamic,
+bool si_array_resize(si_array_t* p_array,
     const size_t new_capacity)
 {
 	bool result = false;
 	// Validate parameter
-	if (NULL == p_dynamic)
+	if (NULL == p_array)
 	{
 		goto END;
 	}
 	// Begin
-	const size_t old_capacity = p_dynamic->capacity;
-	const size_t old_size = (p_dynamic->element_size * old_capacity);
-	const size_t new_size =	(p_dynamic->element_size * new_capacity);
+	const size_t old_capacity = p_array->capacity;
+	const size_t old_size = (p_array->element_size * old_capacity);
+	const size_t new_size =	(p_array->element_size * new_capacity);
 	// Check for overflows
-	if(0u < p_dynamic->element_size)
+	if(0u < p_array->element_size)
 	{
-		if((old_size / p_dynamic->element_size) != old_capacity)
+		if((old_size / p_array->element_size) != old_capacity)
 		{
 			goto END;
 		}
-		if((new_size / p_dynamic->element_size) != new_capacity)
+		if((new_size / p_array->element_size) != new_capacity)
 		{
 			goto END;
 		}
 	}
-	void* tmp = realloc(p_dynamic->p_data, new_size);
+	void* tmp = realloc(p_array->p_data, new_size);
 	if(NULL == tmp)
 	{
 		goto END;
 	}
-	p_dynamic->p_data = tmp;
-	p_dynamic->capacity = new_capacity;
+	p_array->p_data = tmp;
+	p_array->capacity = new_capacity;
 	// If grown, initialize new memory to 0x00(NULL).
 	if(new_size > old_size)
 	{
-		char* p_end = &((char*)p_dynamic->p_data)[old_size];
+		char* p_end = &((char*)p_array->p_data)[old_size];
 		memset(p_end, 0x00, new_size - old_size);
 	}
 	result = true;
@@ -101,179 +119,179 @@ END:
 	return result;
 }
 
-bool si_array_is_pointer_within(const si_array_t* p_dynamic,
+bool si_array_is_pointer_within(const si_array_t* p_array,
 	const void* p_test)
 {
 	bool result = false;
 	// Validate parameters
-	if((NULL == p_dynamic) || (NULL == p_test))
+	if((NULL == p_array) || (NULL == p_test))
 	{
 		goto END;
 	}
-	if(NULL == p_dynamic->p_data)
+	if(NULL == p_array->p_data)
 	{
 		goto END;
 	}
 	// Begin
-	result = (p_test >= p_dynamic->p_data);
+	result = (p_test >= p_array->p_data);
 	if(true == result)
 	{
-		const size_t size = si_array_size(p_dynamic);
-		result = (p_test < (p_dynamic->p_data + size));
+		const size_t size = si_array_size(p_array);
+		result = (p_test < (p_array->p_data + size));
 	}
 	// End
 END:
 	return result;
 }
 
-size_t si_array_find_pointer_offset(const si_array_t* p_dynamic,
+size_t si_array_find_pointer_offset(const si_array_t* p_array,
 	const void* p_test)
 {
 	size_t offset = SIZE_MAX;
 	// Validate parameters
-	if( ! si_array_is_pointer_within(p_dynamic, p_test) )
+	if( ! si_array_is_pointer_within(p_array, p_test) )
 	{
 		// is_pointer_within verifies values are not NULL.
 		goto END;
 	}
 	// Begin
-	const size_t size = si_array_size(p_dynamic);
-	offset = (p_dynamic->p_data + size) - p_test;
+	const size_t size = si_array_size(p_array);
+	offset = (p_array->p_data + size) - p_test;
 	// End
 END:
 	return offset;
 }
 
-bool si_array_is_pointer_element(const si_array_t* p_dynamic,
+bool si_array_is_pointer_element(const si_array_t* p_array,
 	const void* p_test)
 {
 	bool result = false;
 	// Validate parameters
-	if( ! si_array_is_pointer_within(p_dynamic, p_test) )
+	if( ! si_array_is_pointer_within(p_array, p_test) )
 	{
 		// is_pointer_within verifies values are not NULL.
 		goto END;
 	}
-	if(p_dynamic->element_size == 0u)
+	if(p_array->element_size == 0u)
 	{
 		goto END;
 	}
 	// Begin
-	const size_t offset = si_array_find_pointer_offset(p_dynamic, p_test);
-	result = ((offset % p_dynamic->element_size) == 0u);
+	const size_t offset = si_array_find_pointer_offset(p_array, p_test);
+	result = ((offset % p_array->element_size) == 0u);
 	// End
 END:
 	return result;
 }
 
-size_t si_array_find_pointer_index(const si_array_t* p_dynamic,
+size_t si_array_find_pointer_index(const si_array_t* p_array,
 	const void* p_test)
 {
 	size_t index = SIZE_MAX;
 	// Validate parameters
 	// Begin
-	if( ! si_array_is_pointer_element(p_dynamic, p_test))
+	if( ! si_array_is_pointer_element(p_array, p_test))
 	{
 		// is_pointer_element validates values are not NULL.
 		// is_pointer_element validates element_size > 0
 		goto END;
 	}
-	const size_t offset = si_array_find_pointer_offset(p_dynamic, p_test);
-	index = offset / p_dynamic->element_size;
+	const size_t offset = si_array_find_pointer_offset(p_array, p_test);
+	index = offset / p_array->element_size;
 	// End
 END:
 	return index;
 }
 
-void* si_array_at(const si_array_t* p_dynamic,
+void* si_array_at(const si_array_t* p_array,
 	const size_t index)
 {
 	void* p_item = NULL;
 	// Validate Parameters
-	if(NULL == p_dynamic)
+	if(NULL == p_array)
 	{
 		goto END;
 	}
-	if(index >= p_dynamic->capacity)
+	if(index >= p_array->capacity)
 	{
 		goto END;
 	}
 	// Begin
-	p_item = (p_dynamic->p_data + (p_dynamic->element_size * index));
+	p_item = (p_array->p_data + (p_array->element_size * index));
 	// End
 END:
 	return p_item;
 }
-inline void* si_array_first(const si_array_t* p_dynamic)
+inline void* si_array_first(const si_array_t* p_array)
 {
-	return si_array_at(p_dynamic, 0u);
+	return si_array_at(p_array, 0u);
 }
-inline void* si_array_last(const si_array_t* p_dynamic)
+inline void* si_array_last(const si_array_t* p_array)
 {
-	return si_array_at(p_dynamic, p_dynamic->capacity - 1u);
+	return si_array_at(p_array, p_array->capacity - 1u);
 }
 
-void si_array_set(si_array_t* p_dynamic,
+void si_array_set(si_array_t* p_array,
 	const size_t index, const void* p_item)
 {
 	// Validate Parameters
-	if ((NULL == p_dynamic) || (NULL == p_item))
+	if ((NULL == p_array) || (NULL == p_item))
 	{
 		goto END;
 	}
-	const size_t offset = (p_dynamic->element_size * index);
-	const size_t allocated_size = si_array_size(p_dynamic);
+	const size_t offset = (p_array->element_size * index);
+	const size_t allocated_size = si_array_size(p_array);
 	// Bounds Check
 	if(offset >= allocated_size)
 	{
 		goto END;
 	}
 	// Begin
-	memcpy(p_dynamic->p_data + offset, p_item,
-		p_dynamic->element_size);
+	memcpy(p_array->p_data + offset, p_item,
+		p_array->element_size);
 	// End
 END:
 }
 
-void si_array_get(const si_array_t* p_dynamic,
+void si_array_get(const si_array_t* p_array,
 	const size_t index, void* p_item)
 {
 	// Validate parameters
-	if ((NULL == p_dynamic) || (NULL == p_item))
+	if ((NULL == p_array) || (NULL == p_item))
 	{
 		goto END;
 	}
-	const size_t offset = (p_dynamic->element_size * index);
-	const size_t allocated_size = si_array_size(p_dynamic);
+	const size_t offset = (p_array->element_size * index);
+	const size_t allocated_size = si_array_size(p_array);
 	// Bounds Check
 	if(offset >= allocated_size)
 	{
 		goto END;
 	}
 	// Begin
-	memcpy(p_item, p_dynamic->p_data + offset,
-		p_dynamic->element_size);
+	memcpy(p_item, p_array->p_data + offset,
+		p_array->element_size);
 	// End
 	END:
 }
 
-int si_array_cmp(const si_array_t* const p_dynamic_a,
-	const si_array_t* const p_dynamic_b)
+int si_array_cmp(const si_array_t* const p_array_a,
+	const si_array_t* const p_array_b)
 {
 	int result = 0;
-	if(p_dynamic_a == p_dynamic_b)
+	if(p_array_a == p_array_b)
 	{
 		// Pointing to the same struct or same memory.
 		goto END;
 	}
 	// We will treat NULL as the smallest possible value.
-	if(NULL == p_dynamic_a)
+	if(NULL == p_array_a)
 	{
 		// a(0) < b(?)
 		result = -1;
 		goto END;
 	}
-	if(NULL == p_dynamic_b)
+	if(NULL == p_array_b)
 	{
 		// a(?) > b(0)
 		result = 1;
@@ -281,8 +299,8 @@ int si_array_cmp(const si_array_t* const p_dynamic_a,
 	}
 	// Different non-NULL structs in memory.
 	// Determine bounds of data buffer sizes.
-	const size_t a_size = si_array_size(p_dynamic_a);
-	const size_t b_size = si_array_size(p_dynamic_b);
+	const size_t a_size = si_array_size(p_array_a);
+	const size_t b_size = si_array_size(p_array_b);
 	size_t min_size = 0u;
 	size_t max_size = 0u;
 	if(a_size >= b_size)
@@ -300,8 +318,8 @@ int si_array_cmp(const si_array_t* const p_dynamic_a,
 	uint8_t next_b = 0u;
 	for(size_t i = 0u; i < min_size; i++)
 	{
-		next_a = ((uint8_t*)p_dynamic_a->p_data)[i];
-		next_b = ((uint8_t*)p_dynamic_b->p_data)[i];
+		next_a = ((uint8_t*)p_array_a->p_data)[i];
+		next_b = ((uint8_t*)p_array_b->p_data)[i];
 		if(next_a == next_b)
 		{
 			continue;
@@ -335,45 +353,45 @@ END:
 	return result;
 }
 
-void fprint_si_dynamic(FILE* const p_file, const si_array_t* const p_dynamic)
+void fprint_si_array(FILE* const p_file, const si_array_t* const p_array)
 {
 	// Validate parameters
-	if((NULL == p_file) || (NULL == p_dynamic))
+	if((NULL == p_file) || (NULL == p_array))
 	{
 		goto END;
 	}
 	// Begin
 	fprintf(p_file, "{data address: ");
-	if(NULL == p_dynamic->p_data)
+	if(NULL == p_array->p_data)
 	{
 		fprintf(p_file, "NULL, ");
 	}
 	else
 	{
-		fprintf(p_file, "%p, ", p_dynamic->p_data);
+		fprintf(p_file, "%p, ", p_array->p_data);
 	}
-	fprintf(p_file, "element size: %lu, ", p_dynamic->element_size);
-	fprintf(p_file, "capacity: %lu", p_dynamic->capacity);
+	fprintf(p_file, "element size: %lu, ", p_array->element_size);
+	fprintf(p_file, "capacity: %lu", p_array->capacity);
 	fprintf(p_file, "}");
 	// End
 END:
 	return;
 }
 
-void si_array_free(si_array_t* const p_dynamic)
+void si_array_free(si_array_t* const p_array)
 {
 	// Validate parameter
-	if (NULL == p_dynamic)
+	if (NULL == p_array)
 	{
 		goto END;
 	}
 	// Begin
-	if (NULL != p_dynamic->p_data)
+	if (NULL != p_array->p_data)
 	{
-		free(p_dynamic->p_data);
-		p_dynamic->p_data = NULL;
+		free(p_array->p_data);
+		p_array->p_data = NULL;
 	}
-	p_dynamic->capacity = 0u;
+	p_array->capacity = 0u;
 	// End
 END:
 	return;
