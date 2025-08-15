feb04ef771eaa8991bee7482754e12af62006900
ScorpionInc <d2dejohn@gmail.com>
2025-08-15 12:52:54 -0400

    Dump old unit tests for local unit tests.
    Deleted the old project level unit tests for more local library unit tests. Updated CMakeLists to remove project level unit tests and to remove _ prefix.

--- CProjectTemplate/src/si_hashmap_test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-#include "si_hashmap.h"
-
-int main(int arc, char** pp_argv)
-{
-	//TODO
-	return 0;
-}
--- CProjectTemplate/src/si_map_test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-#include "si_map.h"
-
-int main(int argc, char** pp_argv)
-{
-	// TODO
-	return 0;
-}
