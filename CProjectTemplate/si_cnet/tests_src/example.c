1d06cdb292a8da037ec905205c94bd030bd0a714
ScorpionInc <d2dejohn@gmail.com>
2025-08-20 18:11:33 -0400

    Update example.c
    Moved message from handle_input to send_message so other events can reuse that code for announcements when clients connect or leave. Updated example to use new events/event names. Added exit signal handler.

--- CProjectTemplate/si_cnet/tests_src/example.c
+++ CProjectTemplate/si_cnet/tests_src/example.c
@@ -1,12 +1,16 @@
+#include <stdbool.h> // bool, true, false
 #include <stdio.h> // printf()
 #include <ctype.h> // toupper()
 
 #ifdef __linux__
+
 #include <poll.h> // poll()
+
 #else
 #error Unsupported Operating System.
 #endif//__linux__
 
+#include "si_signals.h"
 #include "si_strings.h"
 #include "si_server.h"
 #include "si_paddr.h"
@@ -15,53 +19,149 @@
 #define BUF_SIZE (1024u)
 
 /** Doxygen
- * @brief Handles a single client's input event
- *
- * @param p_unused Pointer to server who owns the socket.
- * @param p_fd Pointer to pollfd struct
+ * @brief Local wrapper for si_server broadcast that prepends a clients address
+ * 
+ * @param p_server Pointer to si_server_t to broadcast message.
+ * @param socket_fd To ID source socket and generate address string from.
+ * @param p_msg Pointer to C String payload to be sent.
+ * 
+ * @return Returns stdbool true on success. Returns false otherwise.
  */
-static void handle_input(struct si_server_t* const p_unused, struct pollfd* const p_fd)
+static bool send_message(struct si_server_t* const p_server,
+	const int socket_fd, const char* const p_msg)
 {
-	if(NULL == p_fd)
+	bool result = false;
+	if((NULL == p_server) || (SOCKET_ERROR >= socket_fd) || (NULL == p_msg))
 	{
+		printf("Send message invalid parameter(s).\n");//!Debugging
 		goto END;
 	}
-	// Handle input
-	char buffer[BUF_SIZE] = {0};
-	ssize_t r_result = read(p_fd->fd, buffer, BUF_SIZE - 1u);
-	if(0 >= r_result)
-	{
-		p_fd->revents |= POLLHUP;
-		goto END;
-	}
-	buffer[r_result] = '\0';
-
 	char* p_message = NULL;
 	size_t message_size = 0u;
 	
 	struct sockaddr_storage peer_address = {0};
 	socklen_t peer_address_len = sizeof(peer_address);
 	const int gpn_result = getpeername(
-		p_fd->fd, (struct sockaddr*)&peer_address, &peer_address_len
+		socket_fd, (struct sockaddr*)&peer_address, &peer_address_len
 	);
+	char* p_tmp = NULL;
 	if(SOCKET_SUCCESS != gpn_result)
+	{
+		p_tmp = strdup("*");
+	}
+	else
+	{
+		p_tmp = sockaddr_as_str((struct sockaddr*)&peer_address);
+		if(NULL == p_tmp)
+		{
+			goto END;
+		}
+	}
+
+	p_message = strv_clone_join(4u, NULL, "[", p_tmp, "]: ", p_msg);
+	free(p_tmp);
+	if(NULL == p_message)
 	{
 		goto END;
 	}
-
-	char* p_tmp = sockaddr_as_str((struct sockaddr*)&peer_address);
-	p_message = strv_clone_join(4u, NULL, "[", p_tmp, "]: ", buffer);
 	message_size = strnlen(p_message, INT64_MAX);
-	free(p_tmp);
 
-	si_server_broadcast(p_unused, (uint8_t*)p_message, message_size, p_fd->fd);
+	si_server_broadcast(p_server, (uint8_t*)p_message, message_size, socket_fd);
 	free(p_message);
+	result = true;
 END:
-	return;
+	return result;
+}
+
+/** Doxygen
+ * @brief Handles a single client's connect event
+ *
+ * @param p_unused Pointer to server who owns the socket.
+ * @param p_fd Pointer to pollfd struct
+ * 
+ * @return Returns true on success. Returns false otherwise.
+ */
+static bool handle_connect(struct si_server_t* const p_server, const int socket_fd)
+{
+	bool result = false;
+	if((NULL == p_server) || (SOCKET_ERROR >= socket_fd))
+	{
+		goto END;
+	}
+	const char* const p_message = "Has joined the server.\n";
+	send_message(p_server, socket_fd, p_message);
+	result = true;
+END:
+	return result;
+}
+
+/** Doxygen
+ * @brief Handles a single client's input event
+ *
+ * @param p_unused Pointer to server who owns the socket.
+ * @param p_fd Pointer to pollfd struct
+ * 
+ * @return Returns true on success. Returns false otherwise.
+ */
+static bool handle_input(struct si_server_t* const p_server, const int socket_fd)
+{
+	bool result = false;
+	if((NULL == p_server) || (SOCKET_ERROR >= socket_fd))
+	{
+		goto END;
+	}
+	// Handle input
+	char buffer[BUF_SIZE] = {0};
+	ssize_t r_result = read(socket_fd, buffer, BUF_SIZE - 1u);
+	if(0 >= r_result)
+	{
+		goto END;
+	}
+	buffer[r_result] = '\0';
+
+	send_message(p_server, socket_fd, buffer);
+	result = true;
+END:
+	return result;
+}
+
+/** Doxygen
+ * @brief Handles a single client's leave event
+ *
+ * @param p_unused Pointer to server who owns the socket.
+ * @param p_fd Pointer to pollfd struct
+ * 
+ * @return Returns true on success. Returns false otherwise.
+ */
+static bool handle_leave(struct si_server_t* const p_server, const int socket_fd)
+{
+	bool result = false;
+	if((NULL == p_server) || (SOCKET_ERROR >= socket_fd))
+	{
+		goto END;
+	}
+	const char* const p_message = "Has left the server.\n";
+	send_message(p_server, socket_fd, p_message);
+	result = true;
+END:
+	return result;
+}
+
+volatile sig_atomic_t g_exit_signal = 0;
+/** Doxygen
+ * @brief Handles graceful exit from an exit signal
+ * 
+ * @param signal Signal code value received.
+ */
+static void handle_exit(const int signal)
+{
+	g_exit_signal++;
 }
 
 int main(int argc, char** pp_argv)
 {
+	set_exit_signal_handler(handle_exit);
+
 	si_logger_t logger = {0};
 	logger.logging_level = SI_LOGGER_ALL;
 	logger.p_file = stdout;
@@ -74,7 +174,9 @@ int main(int argc, char** pp_argv)
 	{
 		goto END;
 	}
-	p_server->p_handle_read = handle_input;
+	p_server->p_on_connect = handle_connect;
+	p_server->p_on_read = handle_input;
+	p_server->p_on_leave = handle_leave;
 
 	si_accesslist_t* p_access = si_accesslist_new(true, true);
 	if(NULL == p_access)
@@ -97,17 +199,26 @@ int main(int argc, char** pp_argv)
 	p_server->access_list = p_access;
 
 	si_server_set_blocking(p_server, false);
-	while(NULL != p_server)
+	while(0 == g_exit_signal)
 	{
 		si_server_accept(p_server);
 		si_server_handle_events(p_server);
 	}
+
+	// Handle cleanup
 	if(NULL != p_access)
 	{
+		si_accesslist_free(p_access);
 		free(p_access);
 		p_access = NULL;
 	}
+	if(NULL != p_loopback)
+	{
+		free(p_loopback);
+		p_loopback = NULL;
+	}
 	si_server_free_at(&p_server);
+	p_server = NULL;
 END:
 	return 0;
 }
