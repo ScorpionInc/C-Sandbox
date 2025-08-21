6c51f302064c200a236fa50c791c3518f39c81c4
ScorpionInc <d2dejohn@gmail.com>
2025-08-21 13:14:56 -0400

    Update test.sh

--- CProjectTemplate/si_thread/test.sh
+++ CProjectTemplate/si_thread/test.sh
@@ -1,6 +1,35 @@
 #!/bin/bash
-echo "Start unit test build script for si_thread."
-gcc -ggdb ./tests_src/si_tasker_test.c ./src/* ./tests_src/unity.c ../si_data/src/* -Itests_include -Iinclude -I../si_data/include -o 'a.out' -lm
-valgrind -s ./a.out
-rm -f ./a.out
-echo "End of unit test build script for si_thread."
+sourceDir="./src/"
+testsDir="./tests_src/"
+outputDir="./build/"
+libName="libsi_thread.a"
+unitySrc="../tests_src/unity.c"
+unityInclude="../tests_include/"
+
+echo 'Building from source via CMake & make.'
+cmake -S . -B "$outputDir"
+cd "$outputDir"
+make
+if [ ! -f "./${libName}" ]; then
+	echo "Library build failed."
+	exit
+fi
+cd ./..
+libPath="${outputDir}${libName}"
+
+echo 'Start of si_thread unit tests'
+tests=$(find "$testsDir" -mindepth 1 -maxdepth 1 -name "*_test.c")
+for test in $tests
+do
+	echo
+	filenameExt="${test##*/}"
+	filename="${filenameExt%.*}"
+	outputFile="${filename}.out"
+	outputFinal="${outputDir}${outputFile}"
+	echo "Building unit test: '$test' -> '$outputFinal'."
+	gcc -ggdb "$test" "$libPath" "../si_data/build/libsi_data.a" "$unitySrc" -I./include -I../si_data/include -I./tests_include -I"$unityInclude" -lm -o "$outputFinal"
+	valgrind -s --log-fd=1 --fair-sched=yes --leak-check=full --show-leak-kinds=all --track-origins=yes "$outputFinal"
+	rm -f "${outputFinal}"
+done
+echo
+echo 'End of si_thread unit tests.'
