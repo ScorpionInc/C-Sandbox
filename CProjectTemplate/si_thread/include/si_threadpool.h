770c735565a72e7a3c3d120946999e6ccc0e4f45
ScorpionInc <d2dejohn@gmail.com>
2025-09-09 16:26:33 -0400

    Update si_threadpool.h
    Adds Doxygen comments to threadpool header.

--- CProjectTemplate/si_thread/include/si_threadpool.h
+++ CProjectTemplate/si_thread/include/si_threadpool.h
@@ -57,16 +57,48 @@ typedef struct si_threadpool_t
 	si_priority_queue_t queue;
 } si_threadpool_t;
 
+/** Doxygen
+ * @brief Initializes an existing si_threadpool at pointer address.
+ * 
+ * @param p_pool Pointer to si_threadpool_t to be initialized.
+ * @param priority_count Number of priority levels to allow.
+ */
 void si_threadpool_init_2(si_threadpool_t* const p_pool,
 	const size_t priority_count);
 void si_threadpool_init  (si_threadpool_t* const p_pool);
 
+/** Doxygen
+ * @brief Allocates and initializes a new si_threadpool_t on the heap.
+ * 
+ * @param priority_count Number of priority levels to allow.
+ * 
+ * @return Returns heap pointer on success. Returns NULL otherwise.
+ */
 si_threadpool_t* si_threadpool_new_1(const size_t priority_count);
 si_threadpool_t* si_threadpool_new  ();
 
+/** Doxygen
+ * @brief Determines if a task result is returned in a threadpool.
+ * 
+ * @param p_pool Pointer to the thread pool struct to check.
+ * @param task_id UID of the task to check for result of.
+ * 
+ * @return Returns stdbool true if found. Returns false otherwise.
+ */
 bool si_threadpool_has_result(si_threadpool_t* const p_pool,
 	const size_t task_id);
 
+/** Doxygen
+ * @brief Enqueues a new task into the priority queue of a thread pool struct.
+ * 
+ * @param p_pool Pointer to the thread pool struct to add task to.
+ * @param p_task Function of the task to be executed. void* func(void* p_arg){}
+ * @param p_parameter Pointer parameter to pass to the task function on run.
+ * @param one_shot Flag determines if automatically restarted. (new id ignored)
+ * @param priority QoS size_t priority level of the task. 0->(priority_count-1)
+ * 
+ * @return Returns task UID on success. Otherwise SI_THREADPOOL_TASK_ID_INVALID
+ */
 size_t si_threadpool_enqueue_5(si_threadpool_t* const p_pool,
 	p_task_f const p_task, void* const p_parameter, const bool one_shot,
 	const size_t priority);
@@ -77,24 +109,67 @@ size_t si_threadpool_enqueue_3(si_threadpool_t* const p_pool,
 size_t si_threadpool_enqueue  (si_threadpool_t* const p_pool,
 	p_task_f const p_task);
 
+/** Doxygen
+ * @brief Pops a task result by UID from threadpool.
+ * 
+ * @param p_pool Pointer to the thread pool struct to pop from.
+ * @param task_id UID of size_t to id the results to be returned.
+ * 
+ * @return Returns result pointer on Success. Returns NULL otherwise.
+ */
 void* si_threadpool_pop_results(si_threadpool_t* const p_pool,
 	const size_t task_id);
 
+/** Doxygen
+ * @brief Blocks waiting for a task result to become ready. (Pops task result)
+ * 
+ * @param p_pool Pointer to the thread pool struct to read from.
+ * @param task_id UID of size_t to id the results to be waited on.
+ * 
+ * @return Returns result pointer on Success. Returns NULL otherwise.
+ */
 void* si_threadpool_await_results(si_threadpool_t* const p_pool,
 	const size_t task_id);
 
+/** Doxygen
+ * @brief Blocks waiting for the pool to be stopped or freed.
+ * 
+ * @param pp_pool Pointer to the heap si_threadpool struct to watch.
+ */
 void si_threadpool_await(si_threadpool_t** const pp_pool);
 
+/** Doxygen
+ * @brief Starts the threadpool's worker threads of a specified count.
+ * 
+ * @param p_pool Pointer to the thread pool struct to start threads in.
+ * @param thread_count Target number of worker threads to start.
+ */
 void si_threadpool_start_2(si_threadpool_t* const p_pool,
 	const size_t thread_count);
 void si_threadpool_start  (si_threadpool_t* const p_pool);
 
+/** Doxygen
+ * @brief Stops a started/running threadpool at address with a timeout.
+ * 
+ * @param p_pool Pointer to the thread pool struct to stop threads of.
+ * @param timeout time_t offset in secs before kill is signaled. 0 = no timeout
+ */
 void si_threadpool_stop_2(si_threadpool_t* const p_pool,
 	const time_t timeout);
 void si_threadpool_stop  (si_threadpool_t* const p_pool);
 
+/** Doxygen
+ * @brief Stops and frees the contents of an existing si_threadpool_t struct.
+ * 
+ * @param p_pool Pointer to the thread pool struct to have it's contents freed.
+ */
 void si_threadpool_free(si_threadpool_t* const p_pool);
 
+/** Doxygen
+ * @brief Stops & frees a heap si_threadpool_t struct by it's pointers address.
+ * 
+ * @param pp_pool Pointer to the thread pool struct's heap pointer to destroy.
+ */
 void si_threadpool_destroy(si_threadpool_t** const pp_pool);
 
 #endif // SI_THREADPOOL_H
