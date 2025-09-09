6b4784b0ff242e11f37d1e18eda0df3ac036b7f1
ScorpionInc <d2dejohn@gmail.com>
2025-09-09 15:49:25 -0400

    Update si_threadpool_test.c
    Add test task that returns a value.

--- CProjectTemplate/si_thread/tests_src/si_threadpool_test.c
+++ CProjectTemplate/si_thread/tests_src/si_threadpool_test.c
@@ -26,15 +26,31 @@ static void* example_task(si_logger_t* p_param)
 {
 	si_logger_debug(p_param, "Example task has started.");
 	// Do work
+	sleep(3);
 	si_logger_debug(p_param, "Example task has stopped.");
 	return NULL;
 }
 
 static void* looping_task(si_logger_t* p_param)
 {
-	si_logger_warning(p_param, "DRINK WATER!");
-	sleep(60);
+	si_logger_warning(p_param, "Drink Water!");
+	sleep(15);
 	return NULL;
+}
+
+static void* returns_value_task(int* p_num)
+{
+	if(NULL == p_num)
+	{
+		goto END;
+	}
+	const int divisor = 42u;
+	while(divisor <= *p_num)
+	{
+		*p_num -= divisor;
+	}
+END:
+	return p_num;
 }
 
 static void handle_signal(int signal)
@@ -60,17 +76,29 @@ static void si_threadpool_test_run(void)
 	const size_t etask_id = si_threadpool_enqueue_4(
 		p_threadpool, (p_task_f)example_task, p_logger, true
 	);
-	printf("Example Task ID: %lu.\n", etask_id);
+	si_logger_debug(p_logger, "Example Task ID: %lu.", etask_id);
 	TEST_ASSERT_NOT_EQUAL_size_t(SI_THREADPOOL_TASK_ID_INVALID, etask_id);
 
 	const size_t ltask_id = si_threadpool_enqueue_5(
 		p_threadpool, (p_task_f)looping_task, p_logger,
 		false, (priority_count - 1u)
 	);
-	printf("Looping Task first ID: %lu.\n", ltask_id);
+	si_logger_debug(p_logger, "Looping Task first ID: %lu.", ltask_id);
 	TEST_ASSERT_NOT_EQUAL_size_t(SI_THREADPOOL_TASK_ID_INVALID, ltask_id);
 	
+	int int_parameter = INT_MAX;
+	const size_t rtask_id = si_threadpool_enqueue_5(
+		p_threadpool, (p_task_f)returns_value_task, &int_parameter,
+		true, (priority_count - 1u)
+	);
+	si_logger_debug(p_logger, "Returns Task ID: %lu. Original: %d.", rtask_id, int_parameter);
+	TEST_ASSERT_NOT_EQUAL_size_t(SI_THREADPOOL_TASK_ID_INVALID, rtask_id);
+
+	si_logger_info(p_logger, "Waiting for results.");
+	si_threadpool_await_results(p_threadpool, rtask_id);
+	si_logger_info(p_logger, "Got task results! New value: %d.", int_parameter);
 	si_threadpool_await(&p_threadpool);
+	si_logger_info(p_logger, "No longer awaiting.");
 
 	si_logger_destroy(&p_logger);
 	TEST_ASSERT_NULL(p_logger);
@@ -89,9 +117,6 @@ static void si_threadpool_test_all(void)
 int main(void)
 {
 	printf("Begin testing of si_threadpool.\n");
-	printf("si_threadpool_t size: %lu.\n", sizeof(si_threadpool_t));
-	printf("si_parray_t size: %lu.\n", sizeof(si_parray_t));
-	printf("si_priority_queue_t size: %lu.\n", sizeof(si_priority_queue_t));
 	si_threadpool_test_all();
 	printf("End of si_threadpool testing.\n");
 	return 0;
