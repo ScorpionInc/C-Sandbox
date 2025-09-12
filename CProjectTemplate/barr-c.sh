4428285fc7f30620bf288503c7f8bfcdad68f156
ScorpionInc <d2dejohn@gmail.com>
2025-09-12 10:40:09 -0400

    Update barr-c.sh
    Make it ANSI pretty. Add a TODO for variable length.

--- CProjectTemplate/barr-c.sh
+++ CProjectTemplate/barr-c.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
+COLOR='\e[1;33m'
+UPBAR='\e[1;53m'
+RESET='\e[0m'
+
+printf "${COLOR}\n"
+echo "################################################################################"
+echo "#                          Start of BARR-C check script.                       #"
+echo "# This script makes no guarantee that all matches are correct or inclusive.    #"
+echo "# This is meant to work as an assistant while performing C static analysis.    #"
+echo "################################################################################"
+printf "${RESET}\n"
+
+printf "${UPBAR}"
 echo "Checking available tools"
+printf "${RESET}"
 if command -v grep &> /dev/null; then
 	echo "Validated 'grep' is available."
 else
@@ -26,73 +40,109 @@ else
 fi
 echo
 
+printf "${UPBAR}"
 echo "Checking Author Alias"
+printf "${RESET}"
 grep -n --binary-files=without-match --ignore-case -R -E '^\s+([*]|//)\s+Author: (\w\s*)+' |
 grep -v -E '(CMake|build[.]sh:|[.]git/|unity(_internals[.](c|h)|-src/|[.](c|h)))' | cut -f1,2 -d':' | sort --unique
 echo
 
+printf "${UPBAR}"
 echo "Checking for { inlining"
+printf "${RESET}"
 grep -n --binary-files=without-match -R -E '(if|while|for)[ ]*[(].*[)][ ]*[{]$' |
 grep -v -E '((CMake|[.]cmake[:])|build[.]sh:|[.]git/|unity(_internals[.](c|h)|-src/|[.](c|h)))' |
 cut -f1,2 -d':' | sort --unique
 echo
 
+printf "${UPBAR}"
 echo "Checking for common Yoda Errors"
+printf "${RESET}"
 grep -n --binary-files=without-match -R -E '(if|while)[ ]?[(].*(==|!=|<[^<]|>[^>]|<=|>=|\|\|)[ ]*(NULL|0[ulf]?|1[ulf]?|true|false|"[\]0"|'\''[\]0'\'')[ ]*[)]' |
 grep -v -E '((CMake|[.]cmake[:])|build[.]sh:|[.]git/|unity(_internals[.](c|h)|-src/|[.](c|h)))' |
 grep -E '[.](c|h):' |
 cut -f1,2 -d':' | sort --unique
 echo
 
+printf "${UPBAR}"
 echo "Checking for pointer p_ prefix"
+printf "${RESET}"
 #-P ^\s*((const|volatile)\s+)?([a-zA-Z_]+[0-9]*[a-zA-Z_]*)[*]+\s+(const\s+([^p\s]|[p]+[^p_\s])|((?!const\s+)[^p\s]|[p]+[^p_\s]))[^*]+
 grep -n --binary-files=without-match -R -P '^\s*((const|volatile)\s+)?([a-zA-Z_]+[0-9]*[a-zA-Z_]*)[*]+\s+(const\s+([^p\s]|[p]+[^p_\s])|((?!const\s+)[^p\s]|[p]+[^p_\s]))[^*]+' | grep -v -E '([(]|[)])' |
 grep -v -E '((CMake|[.]cmake[:])|build[.]sh:|[.]git/|unity(_internals[.](c|h)|-src/|[.](c|h)))' |
 cut -f1,2 -d':' | sort --unique
 echo
 
+printf "${UPBAR}"
 echo "Checking for type _t suffix"
+printf "${RESET}"
 grep -n --binary-files=without-match -R -P '^\s*typedef\s+' |
 grep -v -E '[a-zA-Z]+[a-zA-Z0-9]*(_t)' |
 grep -v -E '((CMake|[.]cmake[:])|build[.]sh:|[.]git/|unity(_internals[.](c|h)|-src/|[.](c|h)))' |
 cut -f1,2 -d':' | sort --unique
 echo
 
+printf "${UPBAR}"
 echo "Checking for conditional defines"
+printf "${RESET}"
 grep -n --binary-files=without-match -R -E '(if|while)[ ]*[(].*[^ (*][(].*[)].*[)]' |
 grep -v -E '((CMake|[.]cmake[:])|build[.]sh:|[.]git/|unity(_internals[.](c|h)|-src/|[.](c|h)))' |
 cut -f1,2 -d':' | sort --unique
 echo
 
+printf "${UPBAR}"
 echo "Checking for conditionals without a comparison operator"
+printf "${RESET}"
 grep -n --binary-files=without-match -R -E '(if|while)[ ]*[(].*[)]' | grep -v -E '(if|while)[ ]*[(].*(==|!=|<[^<]|>[^>]|<=|>=|\|\|).*[)]' |
 grep -v -E '((CMake|[.]cmake[:])|build[.]sh:|[.]git/|unity(_internals[.](c|h)|-src/|[.](c|h)))' |
 grep -E '[.](c|h):' |
 cut -f1,2 -d':' | sort --unique
 echo
 
+printf "${UPBAR}"
+echo "Checking for variables less than 3 in length."
+printf "${RESET}"
+# WIP / TODO
+echo
+
+printf "${UPBAR}"
 echo "Checking for multiple returns"
+printf "${RESET}"
 pcregrep -n --binary-files=without-match -r -M '^(?!extern)\{([^}"]*return[( ][^}"]*[;][^}]*){2,}\}' . |
 grep -v -E '((CMake|[.]cmake[:])|build[.]sh:|[.]git/|unity(_internals[.](c|h)|-src/|[.](c|h)))' |
 grep -E ".*[:][0-9]+[:]" |
 cut -f1,2 -d':' | sort --unique
 echo
 
+printf "${UPBAR}"
 echo "Checking Not explicit cast(s)"
+printf "${RESET}"
 grep -n --binary-files=without-match -R -E '= to(lower|upper)[(]' |
 grep -v -E '((CMake|[.]cmake[:])|build[.]sh:|[.]git/|unity(_internals[.](c|h)|-src/|[.](c|h)))' |
 cut -f1,2 -d':' | sort --unique
 echo
 
+printf "${UPBAR}"
 echo "Checking Uninitialized structs"
+printf "${RESET}"
 grep -n --binary-files=without-match -R '{}' | grep -E '[.](c|h):' |
 grep -v -E '((CMake|[.]cmake[:])|build[.]sh:|[.]git/|unity(_internals[.](c|h)|-src/|[.](c|h)))' |
 grep -v setUp | grep -v tearDown |
 cut -f1,2 -d':' | sort --unique
 echo
 
+printf "${UPBAR}"
 echo "Checking BARR-C Line length"
+printf "${RESET}"
 grep -n --binary-files=without-match -R '.\{80\}' | grep -E '[.](c|h):' |
 grep -v -E '((CMake|[.]cmake[:])|build[.]sh:|[.]git/|unity(_internals[.](c|h)|-src/|[.](c|h)))' |
 cut -f1,2 -d':' | sort --unique
 echo
+
+printf "${COLOR}"
+echo "################################################################################"
+echo "#                           End of BARR-C check script.                        #"
+echo "################################################################################"
+printf "${RESET}"
+echo
+
