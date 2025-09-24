992c15a910e463cc2611fe1f240c6aa7d842b4af
ScorpionInc <d2dejohn@gmail.com>
2025-09-24 16:18:44 -0400

    Update si_targa.h
    Adds Doxygen comments.

--- CProjectTemplate/si_image/include/si_targa.h
+++ CProjectTemplate/si_image/include/si_targa.h
@@ -88,22 +88,73 @@ typedef struct SI_PACKED_INLINE si_tga_header_t
 } si_tga_header_t;     //18
 SI_PACKED_POST
 
+/** Doxygen
+ * @brief Initializes an existing si_tga_header_t struct in memory.
+ * 
+ * @param p_header Pointer to the struct to be initialized.
+ * @param width Initial width value of the header.
+ * @param height Initial height value of the header.
+ * @param channels Desired number of 8-bit color channels from 0->4.
+ */
 void si_tga_header_init_4(si_tga_header_t* const p_header,
 	const uint16_t width, const uint16_t height, const uint8_t channels);
 void si_tga_header_init_3(si_tga_header_t* const p_header,
 	const uint16_t width, const uint16_t height);
 void si_tga_header_init(si_tga_header_t* const p_header);
 
+/** Doxygen
+ * @brief Determines if the pointed at tga header contains valid values.
+ * 
+ * @param p_header Pointer to si_tga_header_t struct to be validated.
+ * 
+ * @return Returns stdbool true if valid. Returns false otherwise.
+ */
 bool si_tga_header_is_valid(const si_tga_header_t* const p_header);
 
+/** Doxygen
+ * @brief Determines the number of 8-bit color channels in a tga header.
+ * 
+ * @param p_header Pointer to si_tga_header_t struct to read from.
+ * 
+ * @return Returns channel count on success. Otherwise returns 0u.
+ */
 uint8_t si_tga_header_channel_count(const si_tga_header_t* const p_header);
 
+/** Doxygen
+ * @brief Returns the pixel data size based upon the values of the tga header.
+ * 
+ * @param p_header Pointer to si_tga_header_t struct to read from.
+ * 
+ * @return Returns size on known fixed size/success. Otherwise returns 0u.
+ */
 size_t si_tga_header_data_size(const si_tga_header_t* const p_header);
 
+/** Doxygen
+ * @brief Reads a tga formatted file's header into header struct from FILE*
+ * 
+ * @param p_header Pointer to si_tga_header_t struct to read header into.
+ * @param p_file FILE pointer to read the binary data from. (mode:'rb')
+ * 
+ * @return Returns a stdbool true on success. Returns false otherwise.
+ */
 bool si_tga_header_fread(si_tga_header_t* const p_header, FILE* const p_file);
 
+/** Doxygen
+ * @brief Writes a tga header from memory to a FILE*
+ * 
+ * @param p_header Pointer to si_tga_header_t to read data from.
+ * @param p_file FILE pointer to write header values to.
+ * 
+ * @return Returns a stdbool true on success. Otherwise returns a false.
+ */
 bool si_tga_header_fwrite(si_tga_header_t* const p_header, FILE* const p_file);
 
+/** Doxygen
+ * @brief Pretty prints the values of an in-memory tga_header to FILE*
+ * 
+ * @param p_file FILE pointer to be written to.
+ * @param p_header Pointer to si_tga_header_t struct to read values from.
+ */
 void si_tga_header_fprint(FILE* const p_file,
 	const si_tga_header_t* const p_header);
 
@@ -121,27 +172,91 @@ typedef struct si_tga_t
 	uint8_t*        p_data;
 } si_tga_t;
 
+/** Doxygen
+ * @brief Initializes the values inside a si_tga_t struct.
+ * 
+ * @param p_tga Pointer to si_tga_t struct to be initialized.
+ * @param width Width of the image to be initialized in pixels.
+ * @param height Height of the image to be initialized in pixels.
+ * @param channels Number of 8-bit color channels in the initial struct.
+ */
 void si_tga_init_4(si_tga_t* const p_tga,
 	const uint16_t width, const uint16_t height, const uint8_t channels);
 void si_tga_init_3(si_tga_t* const p_tga,
 	const uint16_t width, const uint16_t height);
 void si_tga_init(si_tga_t* const p_tga);
 
+/** Doxygen
+ * @brief Heap allocates and initializes a new si_tga_t struct.
+ * 
+ * @return Returns heap pointer on success. Returns NULL otherwise.
+ */
 si_tga_t* si_tga_new();
 
+/** Doxygen
+ * @brief Reads a tga image into memory from a FILE*
+ * 
+ * @param p_tga Pointer to si_tga_t struct to hold values in.
+ * @param p_file FILE pointer to read image data from.
+ * 
+ * @return Returns a stdbool true on success. Returns false otherwise.
+ */
 bool si_tga_fread(si_tga_t* const p_tga, FILE* const p_file);
 
+/** Doxygen
+ * @brief Reads a tga image into memory from a file path.
+ * 
+ * @param p_tga Pointer to the si_tga_t struct to hold values in.
+ * @param p_path C string file path to open and read tga data from.
+ * 
+ * @return Returns stdbool true on success. Returns false otherwise.
+ */
 bool si_tga_fread_from(si_tga_t* const p_tga, const char* const p_path);
 
+/** Doxygen
+ * @brief Finds the location in memory of a pixel by its image position.
+ * 
+ * @param p_tga Pointer to the si_tga_t struct that holds the pixels.
+ * @param x_pos Position of the pixel within the image that is to be found.
+ * @param y_pos Position of the pixel within the image that is to be found.
+ * 
+ * @return Returns pointer to first byte of the pixel value at position or NULL
+ */
 uint8_t* si_tga_pixel_at(const si_tga_t* const p_tga,
 	const uint16_t x_pos, const uint16_t y_pos);
 
+/** Doxygen
+ * @brief Writes a si_tga_t image from memory into a FILE*
+ * 
+ * @param p_tga Pointer to the si_tga_t struct to read the data from.
+ * @param p_file FILE pointer to write the data to.
+ * 
+ * @return Returns stdbool true on success. Returns false otherwise.
+ */
 bool si_tga_fwrite(si_tga_t* const p_tga, FILE* const p_file);
 
+/** Doxygen
+ * @brief Writes a si_tga_t image from memory into a FILE at a file path.
+ * 
+ * @param p_tga Pointer to the si_tga_t struct to read the data from.
+ * @param p_path C string pointer of the file path to write to.
+ * 
+ * @return Returns stdbool true on success. Returns false otherwise.
+ */
 bool si_tga_fwrite_to(si_tga_t* const p_tga, const char* const p_path);
 
+/** Doxygen
+ * @brief Frees allocated data elements within an existing si_tga_t struct.
+ * 
+ * @param p_tga Pointer to the si_tga_t struct to free the contents of.
+ */
 void si_tga_free(si_tga_t* const p_tga);
 
+/** Doxygen
+ * @brief Frees an allocated heap si_tga_t by it's pointer.
+ * 
+ * @param pp_tga Pointer to si_tga_t heap pointer to be freed.
+ */
 void si_tga_destroy(si_tga_t** pp_tga);
 
 #endif//SI_TARGA_H
