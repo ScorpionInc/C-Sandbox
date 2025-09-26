39ee0fe9855b28d63bcee1bd232a87af5c96ef36
ScorpionInc <d2dejohn@gmail.com>
2025-09-26 12:06:07 -0400

    Update si_adler_test.c
    Now uses unity asserts.

--- CProjectTemplate/si_data/tests_src/si_adler_test.c
+++ CProjectTemplate/si_data/tests_src/si_adler_test.c
@@ -14,34 +14,46 @@ void tearDown (void)
 
 void si_adler_test_main(void)
 {
-	// TODO Use the expected results to validate.
 	const char* const p_inputs[] =
 	{
-		"",
 		"~~~",
 		"abc",
 		"abd",
 		"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
 	};
-	const char* const p_expected[] =
+	const uint16_t p_expected[] =
 	{
-		"0x0001",
-		"0x0680",
-		"0x572C",
-		"0x582D",
-		"0xA68B"
+		0x0680,
+		0x572C,
+		0x582D,
+		0xA68B
 	};
-	const size_t inputs_count = 5u;
+	const size_t inputs_count = sizeof(p_expected) / sizeof(uint16_t);
 
 	si_adler_t hash = (si_adler_t){0};
+
+	// Test empty case
+	si_adler_new(&hash, 2u);
+	si_adler_update(&hash, (uint8_t*)"", 0u);
+	const uint16_t raw_result = *((uint16_t*)hash.p_hash);
+	si_adler_free(&hash);
+	TEST_ASSERT_EQUAL_UINT16(0x0001, raw_result);
+
+	// Tests inputs vs expected outputs
 	for (size_t iii = 0u; iii < inputs_count; iii++)
 	{
 		si_adler_new(&hash, 2u);
-		size_t input_length = strlen(p_inputs[iii]);
+		size_t input_length = strnlen(p_inputs[iii], __INT_MAX__);
+		TEST_ASSERT_GREATER_THAN_size_t(0u, input_length);
+		TEST_ASSERT_LESS_THAN_size_t(__INT_MAX__, input_length);
 		si_adler_update(&hash, (uint8_t*)p_inputs[iii], input_length);
+
 		printf("'%48s':", p_inputs[iii]);
 		si_adler_fprint(&hash, stdout); printf("\n");
+		
+		const uint16_t raw_result = *((uint16_t*)hash.p_hash);
 		si_adler_free(&hash);
+		TEST_ASSERT_EQUAL_UINT16(p_expected[iii], raw_result);
 	}
 }
 
