f0635ab895de02cb2c50bd5def69238030c51861
ScorpionInc <d2dejohn@gmail.com>
2025-10-01 13:27:16 -0400

    Create si_thread_t for cross-platform threads

---
+++ CProjectTemplate/si_thread/include/si_thread.h
@@ -0,0 +1,103 @@
+/* si_thread.h
+ * Language: C
+ * Created : 20251001
+ * Purpose : Generalize some fo the common thread functions/structures for
+ *           better cross-platform support.
+*/
+
+#define SI_PTHREAD_SUCCESS (0)
+#define SI_PTHREAD_ERROR   (-1)
+
+#ifdef _WIN32
+
+#include <handleapi.h> // CloseHandle()
+#include <processthreadsapi.h> // CreateThread(), GetThreadId()
+#include <sysinfoapi.h> // GetSystemInfo(), SYSTEM_INFO
+#include <synchapi.h> // WaitForSingleObject(), WaitForSingleObjectEx()
+
+#elif defined(__APPLE__) || defined(__linux__) || defined(__unix__)
+
+#ifndef _POSIX_C_SOURCE
+// Define default minimum POSIX Feature version
+#define _POSIX_C_SOURCE 200809L
+#endif//_POSIX_C_SOURCE
+
+#if defined(__linux__) || defined(__unix__)
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif//_GNU_SOURCE
+#endif// Linux or Unix(FreeBSD)
+
+#include <pthread.h> // pthread_create()
+#include <signal.h> // pthread_kill()
+#include <unistd.h> // sysconf, usleep
+
+#if defined(__APPLE__)
+int pthread_timedjoin(pthread_t thread, void** pp_result,
+	struct timespec* p_timeout);
+#else
+// Linux or Unix(FreeBSD)
+#define pthread_timedjoin(thread, pp_result, p_timeout) \
+	pthread_timedjoin_np(thread, pp_result, p_timeout)
+#endif
+
+#else
+#warning Unknown/Unsupported Operating System.
+#endif // OS Test
+
+#include <stdbool.h> // bool, false, true
+#include <stdint.h> // uint32_t
+
+#ifdef _WIN32
+
+#define si_thread_t HANDLE
+#define si_thread_id_t DWORD
+#define si_thread_func_t DWORD WINAPI
+#define si_thread_func_return_t DWORD
+
+#define si_thread_create(thread, func, p_arg) \
+	(thread = CreateThread(NULL, 0, func, p_arg, 0, NULL))
+#define si_thread_get_id(thread) (GetThreadId(thread))
+
+#define si_thread_is_valid(thread) \
+	(NULL != thread)
+#define si_thread_is_running(thread) \
+	(WAIT_TIMEOUT == WaitForSingleObject(thread, 0))
+
+#define si_thread_free(thread) \
+	((void)CloseHandle(thread); thread = INVALID_HANDLE_VALUE;)
+
+#elif defined(__APPLE__) || defined(__linux__) || defined(__unix__)
+
+#define si_thread_t pthread_t
+#define si_thread_id_t unsigned long
+#define si_thread_func_t void*
+#define si_thread_func_return_t void*
+
+#define si_thread_create(thread, func, p_arg) \
+	((void)pthread_create(thread, NULL, func, p_arg))
+#define si_thread_get_id(thread) ((si_thread_id_t)thread)
+
+#define si_thread_is_valid(thread) \
+	(SI_PTHREAD_SUCCESS == pthread_kill(thread, 0))
+// TODO This is not 100% accurate for detached pthreads.
+// Unlike windows threads, pthreads, after they are created, start
+//    immediately and remain "running" until they are dead/joined.
+#define si_thread_is_running(thread) \
+	(si_thread_is_valid(thread))
+
+#define si_thread_free(thread) // NOP
+
+#endif // OS specific defines
+
+
+#ifndef SI_THREAD_H
+#define SI_THREAD_H
+
+size_t si_cpu_core_count();
+
+si_thread_func_return_t si_thread_join(si_thread_t* const p_thread);
+si_thread_func_return_t si_thread_timedjoin(si_thread_t* const p_thread,
+	const uint32_t millisecs);
+
+#endif//SI_THREAD_H
---
+++ CProjectTemplate/si_thread/src/si_thread.c
@@ -0,0 +1,183 @@
+// si_thread.c
+#include "si_thread.h"
+
+#if defined(__APPLE__)
+// Adds support for Mac OS pthreads timedjoin
+
+typedef struct timedjoin_t
+{
+	bool            was_joined;
+	pthread_t       thread;
+	pthread_mutex_t mutex;
+	pthread_cond_t  signal;
+	void**          pp_result;
+} timedjoin_t;
+
+static void* timed_joiner(void* p_arg)
+{
+	if(NULL == p_arg)
+	{
+		goto END;
+	}
+	timedjoin_t* p_data = (timedjoin_t*)p_arg;
+	(void)pthread_join(p_data->thread, p_data->pp_result);
+	int lock_result = -1;
+	while (0 != lock_result)
+	{
+		lock_result = pthread_mutex_lock(&(p_data->mutex));
+	}
+	p_data->was_joined = true;
+	int unlock_result = -1;
+	while(0 != unlock_result)
+	{
+		unlock_result = pthread_mutex_lock(&(p_data->mutex));
+	}
+	pthread_cond_signal(&(p_data->signal));
+END:
+	return p_arg;
+}
+
+int pthread_timedjoin(pthread_t thread, void** pp_result,
+	struct timespec* p_timeout)
+{
+	int result = -1;
+	if((NULL == pp_result) || (NULL == p_timeout))
+	{
+		goto END;
+	}
+	pthread_t   thread = 0UL;
+	timedjoin_t data   = {0};
+
+	pthread_mutex_init(&(data.mutex) , NULL);
+	pthread_cond_init (&(data.signal), NULL);
+	
+	int lock_result = -1;
+	while (0 != lock_result)
+	{
+		lock_result = pthread_mutex_lock(&(data.mutex));
+	}
+
+	result = pthread_create(&thread, NULL, timed_joiner, &data);
+	if(0 != result)
+	{
+		goto END;
+	}
+	result = pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
+	if(0 != cancelable)
+	{
+		goto END;
+	}
+
+	do
+	{
+		result = pthread_cond_timedwait(
+			&(data.signal), &(data.mutex), p_timeout
+		);
+	} while ((result != ETIMEDOUT) && (true != data.was_joined));
+
+	int unlock_result = -1;
+	while(0 != unlock_result)
+	{
+		unlock_result = pthread_mutex_unlock(&(data.mutex));
+	}
+	pthread_cancel(thread);
+	pthread_join(thread, NULL);
+
+	pthread_cond_destroy(&(data.signal));
+	pthread_mutex_destroy(&(data.mutex));
+	result = (data.was_joined ? 0 : result);
+END:
+	return result;
+}
+#endif// __APPLE__
+
+size_t si_cpu_core_count()
+{
+	size_t count = 1u;
+#ifdef _WIN32
+	SYSTEM_INFO sys_info;
+	GetSystemInfo(&sys_info);
+	count = sys_info.dwNumberOfProcessors;
+#elif defined(__APPLE__) || defined(__linux__) || defined(__unix__)
+	const long sysconf_result = sysconf(_SC_NPROCESSORS_ONLN);
+	if(0L >= sysconf_result)
+	{
+		goto END;
+	}
+	count = (size_t)sysconf_result;
+#else
+	// Assumes this is a single-core processor.
+#endif // OS Specific implementation(s)
+END:
+	return count;
+}
+
+si_thread_func_return_t si_thread_join(si_thread_t* const p_thread)
+{
+	si_thread_func_return_t result = (si_thread_func_return_t)0;
+	if(NULL == p_thread)
+	{
+		goto END;
+	}
+	const bool is_valid = si_thread_is_valid(*p_thread);
+	if(true != is_valid)
+	{
+		goto END;
+	}
+#ifdef _WIN32
+	(void)WaitForSingleObject(*p_thread, INFINITE);
+	(void)GetExitCodeThread(*p_thread, &result);
+#elif defined(__APPLE__) || defined(__linux__) || defined(__unix__)
+	(void)pthread_join(*p_thread, &result);
+#endif// OS Specific implementation(s)
+END:
+	return result;
+}
+
+si_thread_func_return_t si_thread_timedjoin(si_thread_t* const p_thread,
+	const uint32_t millisecs)
+{
+	//! TODO
+	si_thread_func_return_t result = (si_thread_func_return_t)0;
+	if(NULL == p_thread)
+	{
+		goto END;
+	}
+	const bool is_valid = si_thread_is_valid(*p_thread);
+	if(true != is_valid)
+	{
+		goto END;
+	}
+#ifdef _WIN32
+	if(0u == millisecs)
+	{
+		result = si_thread_join(p_thread);
+		goto END;
+	}
+	result = WaitForSingleObject(*p_thread, millisecs);
+	if(WAIT_OBJECT_0 != result)
+	{
+		goto END;
+	}
+	(void)GetExitCodeThread(*p_thread, &result);
+#elif defined(__APPLE__) || defined(__linux__) || defined(__unix__)
+	struct timespec abs_time = {0};
+	const int get_time = clock_gettime(CLOCK_REALTIME, &abs_time);
+	if (0 != get_time)
+	{
+		result = si_thread_join(p_thread);
+		goto END;
+	}
+	const long to_nanosecs = (((long)millisecs) * 1000000L);
+	// Handle Overflows
+	if (to_nanosecs / ((long)millisecs) != 1000000L)
+	{
+		result = si_thread_join(p_thread);
+		goto END;
+	}
+	abs_time.tv_nsec += to_nanosecs;
+	(void)pthread_timedjoin(*p_thread, &result, &abs_time);
+#endif// OS Specific implementation(s)
+END:
+	return result;
+}
